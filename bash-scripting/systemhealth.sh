62875665b2f9b4612ff7b98c011a167b3694157d
Aditya Thorat <thorat.aditya@outlook.com>
2023-03-04 18:21:01 +0530

    bash-scripting

---
+++ bash-scripting/systemhealth.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "----------------------------------------"
+echo "Welcome $USER on $HOSTNAME."
+
+FREERAM=$(free -m | grep Mem | awk '{print $7}')
+LOAD=`uptime | awk '{print $10}'`
+ROOTFREE=$(df -h | grep 'mnt' | awk '{print $4}')
+
+echo "----------------------------------------"
+echo "Available free RAM is $FREERAM MB"
+echo "----------------------------------------"
+echo "Current load average $LOAD"
+echo "----------------------------------------"
+echo "Free ROOT partition size is $ROOTFREE"
+echo "----------------------------------------"
---
+++ bash-scripting/websetup.sh
@@ -0,0 +1,62 @@
+: '
+sudo -i
+
+cat /etc/redhat-release
+
+cd /opt/
+mkdir scripts
+cd scripts
+
+vi websetup.sh
+'
+
+#!/bin/bash
+
+# This sript will setup sloopa website on CENTOS 7.5 HTTPD service.
+
+
+# Declaring Variables
+SVC=httpd
+WEBURL=https://www.tooplate.com/zip-templates/2104_sloopa.zip
+PACKS='httpd wget unzip'
+
+
+# Setup packages
+echo "----------------------------------------"
+echo "Installing Packages"
+echo '----------------------------------------'
+yum install -y $PACKS
+echo
+
+
+# Start & Enable Service
+echo "----------------------------------------"
+echo "Starting Services"
+echo '----------------------------------------'
+systemctl start $SVC
+systemctl enable $SVC
+echo
+
+
+# Copy website data to apache Doc Root Dir.
+echo "----------------------------------------"
+echo "Copying Website Data"
+echo '----------------------------------------'
+cd /tmp && wget -O website.zip $WEBURL
+cd /tmp && unzip website.zip
+cp -r /tmp/2104_sloopa/* /var/www/html/
+echo
+
+
+# Restart Service
+echo "----------------------------------------"
+echo "Restarting Services"
+echo '----------------------------------------'
+systemctl restart $SVC
+echo
+
+
+: '
+chmod u+x websetup.sh
+./websetup.sh
+'
